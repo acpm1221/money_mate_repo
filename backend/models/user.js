bb80a3d8c828119f2149ac6e38d53221b1bfa806
Anmol Kumar <132394800+acpm1221@users.noreply.github.com>
2025-04-28 00:09:34 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        unique: true,
+    name: { type: String, required: true },
+    email: { 
+        type: String, 
+        unique: true, 
         required: true,
         match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/
     },
-    password: {
-        type: String,
-        required: true
-    },
-    profilePic: {
-        type: String
-    }
+    password: { type: String, required: true },
+    profilePic: { type: String },
+    securityQuestion: { type: String, required: true },
+    securityAnswer: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
