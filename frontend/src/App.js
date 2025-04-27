41a17615bafd0b7d10a8a6baed1823dc8b1c37a8
Anmol Kumar <132394800+acpm1221@users.noreply.github.com>
2025-04-27 05:30:47 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,40 @@ import Signup from './Components/Signup';
 import Dashboard from './Components/Dashboard';
 import Terms from './Components/Terms';
 import AddTransaction from './Components/AddTransaction';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-    const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
 
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login onLogin={() => window.location.href = '/dashboard'} />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/dashboard" element={token ? <Dashboard token={token} /> : <Navigate to="/" />} />
-                <Route path="/add/:type" element={token ? <AddTransaction token={token} /> : <Navigate to="/" />} />
-                <Route path="/terms" element={<Terms />} />
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      {/* Toast Notification Container */}
+      <ToastContainer 
+        position="top-right" 
+        autoClose={3000} 
+        hideProgressBar={false} 
+        newestOnTop={false} 
+        closeOnClick 
+        rtl={false} 
+        pauseOnFocusLoss 
+        draggable 
+        pauseOnHover 
+        theme="colored"
+      />
+
+      <Routes>
+        {/* If token exists, redirect to dashboard */}
+        <Route path="/" element={<Login onLogin={() => window.location.href = '/dashboard'} />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={token ? <Dashboard token={token} /> : <Navigate to="/" />} />
+        <Route path="/add/:type" element={token ? <AddTransaction token={token} /> : <Navigate to="/" />} />
+        <Route path="/terms" element={<Terms />} />
+        {/* Fallback Route */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
